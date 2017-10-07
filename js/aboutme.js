b708b07669fdf188246f622d0efa7e5f83bf62ac
akshay2395 <32189287+akshay2395@users.noreply.github.com>
2017-10-07 11:29:11 +0530

    first

---
+++ js/aboutme.js
@@ -0,0 +1,13 @@
+$("#tabContent").children().siblings().children().find("h3").css("text-align","center");
+
+$("#navbar").css("padding-top","4%");
+
+$("#tabContent").children().siblings().children().siblings().css("padding-top","6%");
+
+$("#bhandup").children().find("img").css("padding-top","8%");
+
+$("#me").children().children().find("img").css({"padding-top":"8%","margin":"auto"});
+
+$("#education").children().children().first().css("padding-top","8%");
+
+$("#keyskills").children().children().first().css("padding-top","8%");
---
+++ js/firstpage.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("button").click(function myFunction(){
+        $("#firstPageButton").slideUp(100).delay(800);          
+            $("#portfolio").css("visibility","visible");
+    });
+});
